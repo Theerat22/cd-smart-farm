cd56bf814ac54ef5136061f696e2a27b7249d62c
Theeratdolchat Chatchai <sitthach7777@gmail.com>
2025-03-19 17:30:24 +0700

    del unused

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -4,25 +4,14 @@ import React, { useState } from 'react';
 import Navbar from '@/app/components/Navbar';
 import OverviewPage from '@/app/dashboard/pages/overview';
 import PlantsDashboard from './pages/plants';
-import PlantGrowthDashboard from './pages/tracking';
-// import Sensor from '@/app/dashboard/pages/realtimedata';
+
 const Dashboard: React.FC = () => {
   const [activePage, setActivePage] = useState('overview');
-
-  const plantData = {
-    id: "plant-01",
-    data: {
-      Plants: [0, 1, 2, 3, 1, 2, 3, 0, 1, 2],
-      TDS: 650,
-      TimeStamp: "2025-03-18T14:30:00"
-    }
-  };
 
   const renderPage = () => {
     switch(activePage) {
       case 'overview': return <OverviewPage />;
       case 'plants': return <PlantsDashboard />;
-      case 'analytics': return <PlantGrowthDashboard plantData={plantData}/>;
       default: return <OverviewPage />;
     }
   };
